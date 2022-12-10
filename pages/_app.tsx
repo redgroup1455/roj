b1369b5908ba9b0f98ac3f64bbbd7e6becb7d761
Oein <oein0219@gmail.com>
2022-12-10 15:36:15 +0900

    add session provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { NextUIProvider } from "@nextui-org/react";
+import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  console.log(`
+                  Welcome to ROJ
+      Developers / Sangho129, Oein, Kalswns
+  `);
   return (
-    <NextUIProvider>
-      <Component {...pageProps} />
-    </NextUIProvider>
+    <SessionProvider>
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </SessionProvider>
   );
 }

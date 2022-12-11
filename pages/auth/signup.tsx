26ce349116a97cf3b2c2b5b6781dbbdc450e70e8
Oein <oein0219@gmail.com>
2022-12-11 10:10:03 +0900

    signup

--- pages/auth/signup.tsx
+++ pages/auth/signup.tsx
@@ -1,15 +1,22 @@
 import { Checkbox, Input, Row, Text, Button, Modal } from "@nextui-org/react";
 import { useState } from "react";
+import ModalPolicy from "../../components/ModalPolicy";
 
 export default function Signin() {
   let [policyOpened, setPolicyOpened] = useState<boolean>(false);
   let [policyAccepted, setPolicyAccepted] = useState<boolean>(false);
 
+  let [errorMessage, setErrorMessage] = useState<string>("");
+  let [acceptBtnEnalbed, setAcceptBtnEnalbed] = useState<boolean>(false);
+
+  let [username, setUsername] = useState<string>("");
+  let [password, setPassword] = useState<string>("");
+
   return (
     <div
       style={{
         backgroundImage:
-          "url(https://www.adobe.com/content/dam/cc/us/en/creative-cloud/photography/discover/landscape-photography/CODERED_B1_landscape_P2d_714x348.jpg.img.jpg)",
+          "url(https://cdn.discordapp.com/attachments/1051010334707691572/1051130577669062696/4ea1221c-78f0-4952-a3c6-f16acb33b57f.png)",
         width: "100vw",
         height: "100vh",
         backgroundRepeat: "no-repeat",
@@ -20,6 +27,9 @@ export default function Signin() {
         open={policyOpened}
         onClose={() => {
           setPolicyOpened(false);
+        }}
+        onOpen={() => {
+          setAcceptBtnEnalbed(false);
         }}
       >
         <Modal.Header>
@@ -33,50 +43,14 @@ export default function Signin() {
               overflowY: "auto",
               maxHeight: "40vh",
             }}
+            onScroll={(e) => {
+              let thid = e.target as HTMLDivElement;
+              if (thid.scrollTop > 900) {
+                setAcceptBtnEnalbed(true);
+              }
+            }}
           >
-            어쩔 이용 약관일 수 도있고 아닐 수 도있스브니다. 어쩔 이용 약관일 수
-            도있고 아닐 수 도있스브니다. 어쩔 이용 약관일 수 도있고 아닐 수
-            도있스브니다. 어쩔 이용 약관일 수 도있고 아닐 수 도있스브니다. 어쩔
-            이용 약관일 수 도있고 아닐 수 도있스브니다. 어쩔 이용 약관일 수
-            도있고 아닐 수 도있스브니다. 어쩔 이용 약관일 수 도있고 아닐 수
-            도있스브니다. 어쩔 이용 약관일 수 도있고 아닐 수 도있스브니다. 어쩔
-            이용 약관일 수 도있고 아닐 수 도있스브니다. 어쩔 이용 약관일 수
-            도있고 아닐 수 도있스브니다. 어쩔 이용 약관일 수 도있고 아닐 수
-            도있스브니다. 어쩔 이용 약관일 수 도있고 아닐 수 도있스브니다. 어쩔
-            이용 약관일 수 도있고 아닐 수 도있스브니다. 어쩔 이용 약관일 수
-            도있고 아닐 수 도있스브니다. 어쩔 이용 약관일 수 도있고 아닐 수
-            도있스브니다. 어쩔 이용 약관일 수 도있고 아닐 수 도있스브니다. 어쩔
-            이용 약관일 수 도있고 아닐 수 도있스브니다. 어쩔 이용 약관일 수
-            도있고 아닐 수 도있스브니다. 어쩔 이용 약관일 수 도있고 아닐 수
-            도있스브니다. 어쩔 이용 약관일 수 도있고 아닐 수 도있스브니다. 어쩔
-            이용 약관일 수 도있고 아닐 수 도있스브니다. 어쩔 이용 약관일 수
-            도있고 아닐 수 도있스브니다. 어쩔 이용 약관일 수 도있고 아닐 수
-            도있스브니다. 어쩔 이용 약관일 수 도있고 아닐 수 도있스브니다. 어쩔
-            이용 약관일 수 도있고 아닐 수 도있스브니다. 어쩔 이용 약관일 수
-            도있고 아닐 수 도있스브니다. 어쩔 이용 약관일 수 도있고 아닐 수
-            도있스브니다. 어쩔 이용 약관일 수 도있고 아닐 수 도있스브니다. 어쩔
-            이용 약관일 수 도있고 아닐 수 도있스브니다. 어쩔 이용 약관일 수
-            도있고 아닐 수 도있스브니다. 어쩔 이용 약관일 수 도있고 아닐 수
-            도있스브니다. 어쩔 이용 약관일 수 도있고 아닐 수 도있스브니다. 어쩔
-            이용 약관일 수 도있고 아닐 수 도있스브니다. 어쩔 이용 약관일 수
-            도있고 아닐 수 도있스브니다. 어쩔 이용 약관일 수 도있고 아닐 수
-            도있스브니다. 어쩔 이용 약관일 수 도있고 아닐 수 도있스브니다. 어쩔
-            이용 약관일 수 도있고 아닐 수 도있스브니다. 어쩔 이용 약관일 수
-            도있고 아닐 수 도있스브니다. 어쩔 이용 약관일 수 도있고 아닐 수
-            도있스브니다. 어쩔 이용 약관일 수 도있고 아닐 수 도있스브니다. 어쩔
-            이용 약관일 수 도있고 아닐 수 도있스브니다. 어쩔 이용 약관일 수
-            도있고 아닐 수 도있스브니다. 어쩔 이용 약관일 수 도있고 아닐 수
-            도있스브니다. 어쩔 이용 약관일 수 도있고 아닐 수 도있스브니다. 어쩔
-            이용 약관일 수 도있고 아닐 수 도있스브니다. 어쩔 이용 약관일 수
-            도있고 아닐 수 도있스브니다. 어쩔 이용 약관일 수 도있고 아닐 수
-            도있스브니다. 어쩔 이용 약관일 수 도있고 아닐 수 도있스브니다. 어쩔
-            이용 약관일 수 도있고 아닐 수 도있스브니다. 어쩔 이용 약관일 수
-            도있고 아닐 수 도있스브니다. 어쩔 이용 약관일 수 도있고 아닐 수
-            도있스브니다. 어쩔 이용 약관일 수 도있고 아닐 수 도있스브니다. 어쩔
-            이용 약관일 수 도있고 아닐 수 도있스브니다. 어쩔 이용 약관일 수
-            도있고 아닐 수 도있스브니다. 어쩔 이용 약관일 수 도있고 아닐 수
-            도있스브니다. 어쩔 이용 약관일 수 도있고 아닐 수 도있스브니다. 어쩔
-            이용 약관일 수 도있고 아닐 수 도있스브니다.
+            <ModalPolicy />
           </div>
         </Modal.Body>
         <Modal.Footer>
@@ -97,6 +71,7 @@ export default function Signin() {
               setPolicyOpened(false);
               setPolicyAccepted(true);
             }}
+            disabled={!acceptBtnEnalbed}
           >
             동의
           </Button>
@@ -142,22 +117,52 @@ export default function Signin() {
             fullWidth
             color="error"
             size="lg"
-            labelPlaceholder="Username"
-            css={{
-              marginBottom: "35px",
+            labelLeft="Username"
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
             }}
           />
+          <div
+            style={{
+              transition: "all .3s ease",
+              height: "0px",
+            }}
+          >
+            <div
+              style={{
+                color: "var(--nextui-colors-error)",
+                transition: "all .3s ease",
+                maxHeight:
+                  errorMessage.length == 0
+                    ? "var(--nextui-fontSizes-xl)"
+                    : "0px",
+              }}
+            >
+              {errorMessage}
+            </div>
+          </div>
           <Input
             clearable
             bordered
             fullWidth
             color="error"
             size="lg"
-            labelPlaceholder="Password"
+            labelLeft="Password"
             css={{
+              marginTop: "10px",
               marginBottom: "15px",
             }}
             type="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            helperText={
+              password.length < 8 && password.length > 0
+                ? "Password must be at least 8 characters"
+                : ""
+            }
           />
           <Row justify="space-between">
             <div
@@ -186,7 +191,13 @@ export default function Signin() {
                 </Text>
               </Checkbox>
             </div>
-            <Button auto color="error" disabled={!policyAccepted}>
+            <Button
+              auto
+              color="error"
+              disabled={
+                !policyAccepted || username.length == 0 || password.length < 8
+              }
+            >
               Next
             </Button>
           </Row>

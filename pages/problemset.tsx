50e0b81a85df6f530d32ebd6136b1586d32b3447
Oein <oein0219@gmail.com>
2022-12-11 16:22:07 +0900

    remove table for now

--- pages/problemset.tsx
+++ pages/problemset.tsx
@@ -5,19 +5,6 @@ export default function problemset() {
   return (
     <div>
       <CustomNavbar />
-      <Table
-        aria-label="Example table with static content"
-        css={{
-          height: "auto",
-          minWidth: "100%",
-        }}
-      >
-        <Table.Header>
-          <Table.Column>번호</Table.Column>
-          <Table.Column>문제</Table.Column>
-          <Table.Column>난이도</Table.Column>
-        </Table.Header>
-      </Table>
     </div>
   );
 }

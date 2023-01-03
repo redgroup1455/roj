a2fc3da78d71eaa50e56a62682838849d341feac
Oein <oein0219@gmail.com>
2023-01-03 11:38:56 +0900

    Navbar update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,29 +1,67 @@
 /* eslint-disable @next/next/no-img-element */
 
-import { Container, Row, Col, Text } from "@nextui-org/react";
+import { Button, Grid } from "@nextui-org/react";
 import Link from "next/link";
-import NoSSR from "react-no-ssr";
+import DarkMode from "../components/DarkMode";
 
 export default function CustomNavbar() {
   return (
-    <div style={{
-      position: "fixed",
-      top: 0,
-      left: 0,
-      right: 0,
-      width: "100vw",
-      height: "48px",
-      borderBottom: "1px solid #ccc"
-    }}>
-      <Container>
-        <Row>
-          <Col>
-            <Text>
-              GOCO
-            </Text>
-          </Col>
-        </Row>
-      </Container>
+    <div
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+        width: "100vw",
+        height: "fit-content",
+        borderBottom: "1px solid #ccc",
+      }}
+    >
+      <Grid.Container>
+        <Grid
+          xs={1}
+          css={{
+            justifyContent: "center",
+          }}
+        >
+          <div
+            style={{
+              textAlign: "center",
+              fontSize: "2rem",
+              padding: "auto",
+              margin: "none",
+            }}
+          >
+            GOCO
+          </div>
+        </Grid>
+        <Grid xs={9}>
+          <Grid.Container>
+            <Grid></Grid>
+          </Grid.Container>
+        </Grid>
+        <Grid xs={2}>
+          <Grid.Container>
+            <Grid xs={2}>
+              <div className="vcAlign">
+                <DarkMode />
+              </div>
+            </Grid>
+            <Grid xs={4}>
+              <Link href="/auth/signin">
+                <div className="vcAlign">Sign in</div>
+              </Link>
+            </Grid>
+            <Grid xs={4}>
+              <Link href="/auth/signup">
+                <Button color="error" auto flat className="vcAlign">
+                  <div>Sign up</div>
+                </Button>
+              </Link>
+            </Grid>
+          </Grid.Container>
+        </Grid>
+      </Grid.Container>
     </div>
   );
 }
